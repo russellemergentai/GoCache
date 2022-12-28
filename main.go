d2eb3e27f3245cd95131b5a941fe5140bec4e897
Russell <52071229+russellemergentai@users.noreply.github.com>
2022-12-28 12:43:37 +0000

    Encrypt cachw

--- main.go
+++ main.go
@@ -1,46 +1,111 @@
 package main
 
 import (
+	"crypto/aes"
+	"encoding/hex"
 	"fmt"
-	// myPackage "main/myPackage"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
-type Human interface {
-  hobby() string
-}
+// cache map
+var data = make(map[string]string)
 
-type Man struct {
-  name string
-}
-
-type Woman struct {
-  name string
-}
-
-// implement the interface fir each struct
-func (m Man) hobby () string {
- return m.name
-}
-
-func (m Woman) hobby () string {
-  return m.name
-}
-      
 func main() {
-  m := new (Man)
-  m.name = "chad"
-  w := new (Woman)
-  w.name = "karen"
 
-  fmt.Println(m.hobby())
-  fmt.Println(w.hobby())
+	// cipher key
+	// key := "thisis32bitlongpassphraseimusing"
 
-  // do polymorphism via interface
+	// pt := "This is a secret"
+	// c := EncryptAES([]byte(key), pt)
+	// fmt.Println(pt)
 
-  people := []Human{m, w}
+	// DecryptAES([]byte(key), c)
+	// fmt.Println(c)
 
-  for _, item := range people {
-    fmt.Printf("%s \n", item.hobby())
-  }
+	insert("key1", "value1")
+	insert("key2", "value2")
+	writeEncrypt()
+	erase()
+	readDecrypt()
+}
 
+func insert(key, value string) {
+	data[key] = value
+}
+
+func deleteByKey(key string) {
+	_, exists := data[key]
+	if exists {
+		delete(data, key)
+	}
+}
+
+func display() {
+	for _, name := range data {
+		fmt.Println(name)
+	}
+}
+
+func erase() {
+	for k := range data {
+		delete(data, k)
+	}
+}
+
+func writeEncrypt() {
+	file, _ := os.Create("data.txt")
+	defer file.Close()
+
+	for key, value := range data {
+		v := fmt.Sprintf("%s#%s", key, value)
+		file.WriteString(v)
+	}
+}
+
+func readDecrypt() {
+	v, _ := ioutil.ReadFile("data.txt")
+	z := string(v)
+	w := strings.Split(z, "\n")
+	fmt.Println(len(z))
+
+	for _, x := range w {
+		// y := strings.Split(x, "#")
+		// k := y[0]
+		// v := y[1]
+		fmt.Println(x)
+	}
+	// display()
+}
+
+func EncryptAES(key []byte, plaintext string) string {
+
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	out := make([]byte, len(plaintext))
+
+	c.Encrypt(out, []byte(plaintext))
+
+	return hex.EncodeToString(out)
+}
+
+func DecryptAES(key []byte, ct string) {
+	ciphertext, _ := hex.DecodeString(ct)
+
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	pt := make([]byte, len(ciphertext))
+	c.Decrypt(pt, ciphertext)
+
+	s := string(pt[:])
+	fmt.Println("DECRYPTED:", s)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
